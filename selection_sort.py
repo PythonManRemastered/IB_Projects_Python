d03327068ff7658d163283bedf8ae163ff3cf098
PythonManRemastered <aadi.chakraborty@gmail.com>
2025-09-08 11:53:15 +0530

    finished selection sorting, used check file to understand how array slicing works

---
+++ selection_sort.py
@@ -0,0 +1,12 @@
+def selection_sort(array, size):
+    for i in range(size):
+        min_index = i
+        for j in range(i+1, size):
+            if array[j] < array[min_index]:
+                min_index = j
+
+        array[i], array[min_index] = array[min_index], array[i]
+    return array
+arr = [1,2,4,3,7,5]
+size = len(arr)
+print(selection_sort(arr, size))
